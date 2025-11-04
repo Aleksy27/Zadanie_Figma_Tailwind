d9347c3525bdb22391e31f49f0a0fc98f79d9c55
Aleksander Piotrowski <aleksanderpiotrowski194@gmail.com>
2025-11-04 09:46:40 +0100

    commit

--- figma_tailwind/src/components/Holder/Holder.tsx
+++ figma_tailwind/src/components/Holder/Holder.tsx
@@ -1,9 +1,37 @@
+import { LuShieldBan } from "react-icons/lu";
+
 export default function Holder() {
   return (
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-      <div>
-        <div className="text-xl font-medium text-black">Holder Component</div>
-        <p className="text-gray-500">This is a reusable holder component styled with Tailwind CSS.</p>
+    <div className="flex flex-wrap gap-5 items-center justify-center">
+      <div className="flex flex-col w-[30%] h-55 border-3 border-green-500 bg-[#242424] rounded-xl p-6 gap-2">
+        <LuShieldBan className="text-green-500 text-5xl bg-green-200 p-1 rounded-sm"/>
+        <h2 className="font-bold text-2xl">Secure Checkout</h2>
+        <p className="font-thin">Enjoy a safe shopping experience with multiple payment options and SSL encryption. Your personal and financial information is always protected.</p>
+      </div>
+      <div className="flex flex-col w-[30%] h-55 border-3 border-green-500 bg-[#242424] rounded-xl p-6 gap-2">
+        <LuShieldBan className="text-green-500 text-5xl bg-green-200 p-1 rounded-sm"/>
+        <h2 className="font-bold text-2xl">Secure Checkout</h2>
+        <p className="font-thin">Enjoy a safe shopping experience with multiple payment options and SSL encryption. Your personal and financial information is always protected.</p>
+      </div>
+      <div className="flex flex-col w-[30%] h-55 border-3 border-green-500 bg-[#242424] rounded-xl p-6 gap-2">
+        <LuShieldBan className="text-green-500 text-5xl bg-green-200 p-1 rounded-sm"/>
+        <h2 className="font-bold text-2xl">Secure Checkout</h2>
+        <p className="font-thin">Enjoy a safe shopping experience with multiple payment options and SSL encryption. Your personal and financial information is always protected.</p>
+      </div>
+      <div className="flex flex-col w-[30%] h-55 border-3 border-green-500 bg-[#242424] rounded-xl p-6 gap-2">
+        <LuShieldBan className="text-green-500 text-5xl bg-green-200 p-1 rounded-sm"/>
+        <h2 className="font-bold text-2xl">Secure Checkout</h2>
+        <p className="font-thin">Enjoy a safe shopping experience with multiple payment options and SSL encryption. Your personal and financial information is always protected.</p>
+      </div>
+      <div className="flex flex-col w-[30%] h-55 border-3 border-green-500 bg-[#242424] rounded-xl p-6 gap-2">
+        <LuShieldBan className="text-green-500 text-5xl bg-green-200 p-1 rounded-sm"/>
+        <h2 className="font-bold text-2xl">Secure Checkout</h2>
+        <p className="font-thin">Enjoy a safe shopping experience with multiple payment options and SSL encryption. Your personal and financial information is always protected.</p>
+      </div>
+      <div className="flex flex-col w-[30%] h-55 border-3 border-green-500 bg-[#242424] rounded-xl p-6 gap-2">
+        <LuShieldBan className="text-green-500 text-5xl bg-green-200 p-1 rounded-sm"/>
+        <h2 className="font-bold text-2xl">Secure Checkout</h2>
+        <p className="font-thin">Enjoy a safe shopping experience with multiple payment options and SSL encryption. Your personal and financial information is always protected.</p>
       </div>
     </div>
   )
